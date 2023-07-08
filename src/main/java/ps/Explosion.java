3eb55f3ba9b59aed55379cc4ed3f9c9674486fe7
mayconsamaral@gmail.com <mayconsamaral@gmail.com>
2023-07-08 16:47:53 -0300

    Agora explode para todos os lados

--- src/main/java/ps/Explosion.java
+++ src/main/java/ps/Explosion.java
@@ -2,22 +2,20 @@ package ps;
 
 import entities.Ator;
 import lombok.SneakyThrows;
+import utilities.GameUtil;
 import utilities.ResourceManager;
 
 import java.awt.*;
 import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
-import java.util.Random;
 
 public class Explosion {
     private final int QTD_PARTES = 6;
     private Particle[] particles;
-    private Random rand;
     private BufferedImage particleImage[];
 
     @SneakyThrows
     public Explosion(Ator ator) {
-        this.rand = new Random();
         this.particles = new Particle[QTD_PARTES];
         this.particleImage = new BufferedImage[QTD_PARTES];
         this.init(ator);
@@ -35,11 +33,11 @@ public class Explosion {
 
     private Particle createParticle(Ator ator) {
 
-        int px = ator.getX() + rand.nextInt(ator.getLargura());
-        int py = ator.getY() + rand.nextInt(ator.getAltura());
+        int px = ator.getX();
+        int py = ator.getY();
 
-        int sx = rand.nextInt(10);
-        int sy = rand.nextInt(10);
+        int sx = GameUtil.getRandomNumber(-10, 10);
+        int sy = GameUtil.getRandomNumber(-10, 10);
 
         return Particle.builder()
                 .position(new Point2D.Float(px, py))
