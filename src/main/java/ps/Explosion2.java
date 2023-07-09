4425f6d65bc59840bd8cd2a0b2bce01b3e08f764
TiagoHucs <tiagohucs@gmail.com>
2023-07-09 16:10:53 -0300

    teste explosion2

---
+++ src/main/java/ps/Explosion2.java
@@ -0,0 +1,56 @@
+package ps;
+
+import entities.Ator;
+import game.GameComponent;
+import lombok.SneakyThrows;
+import utilities.GameUtil;
+import utilities.ResourceManager;
+
+import java.awt.*;
+import java.awt.geom.Point2D;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Explosion2 {
+    List<BufferedImage> sprites = new ArrayList<>(8);
+
+    Ator ator = null;
+
+    int index = 0;
+    List<String> srcList = new ArrayList<>(8);
+
+    @SneakyThrows
+    public Explosion2(Ator ator) {
+        this.init(ator);
+        this.ator = ator;
+
+        for (int i = 1 ; i <= 8; i++) {
+            srcList.add("/image/Explosion/explosion-" + i + ".png");
+        }
+
+        for (String src : srcList) {
+            sprites.add(ResourceManager.get().getImage(src,
+                    ator.getLargura(),
+                    ator.getAltura()));
+        }
+    }
+
+    @SneakyThrows
+    public void init(Ator ator) {
+
+    }
+
+    public void update(Graphics g, GameComponent game) {
+        if(index < 8){
+            System.out.println("index: " +index);
+            g.drawImage(sprites.get(index),ator.getX(),ator.getY(),game);
+        }
+        index++;
+    }
+
+    public boolean isFinished() {
+        return index > 8;
+    }
+
+}
