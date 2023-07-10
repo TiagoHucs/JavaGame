4401aebc236dfa4a2a408f1dedf7e2db58a418c4
TiagoHucs <tiagohucs@gmail.com>
2023-07-10 12:10:56 -0300

    teste logo

--- src/main/java/menu/IntroMenu.java
+++ src/main/java/menu/IntroMenu.java
@@ -2,33 +2,41 @@ package menu;
 
 import game.GameComponent;
 import game.GameState;
+import utilities.ResourceManager;
 
 import java.awt.*;
 import java.awt.event.KeyEvent;
+import java.io.IOException;
 
 public class IntroMenu extends AbstractGameMenu {
     private GameComponent gameComponent;
+    private Image logo;
 
     private Graphics g;
     public IntroMenu(GameComponent gameComponent) {
         super(gameComponent);
         this.gameComponent = gameComponent;
+        try {
+            this.logo = ResourceManager.get().getImage("/image/logo.png");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void draw(Graphics g, GameComponent gameComponent) {
         this.g = g;
-        this.write(Color.YELLOW,"INVADERS",40);
-        this.write(Color.WHITE,"Press any key",20);
+        int x = (gameComponent.getCfg().getLarguraTela() - logo.getWidth(gameComponent)) / 2;
+        this.g.drawImage(logo,x, gameComponent.getCfg().getAlturaTela()/3, null,gameComponent);
+        this.write(Color.WHITE,"Press any key",20, gameComponent.getCfg().getAlturaTela()-200);
     }
 
-    private void write(Color color,String text, int size){
+    private void write(Color color,String text, int size, int y){
         this.font = new Font("Arial", Font.PLAIN, size);
         this.g.setFont(font);
         this.metrics = this.g.getFontMetrics(font);
         int larguraTexto = metrics.stringWidth(text);
         int x = (gameComponent.getCfg().getLarguraTela() - larguraTexto) / 2;
-        int y = (gameComponent.getCfg().getAlturaTela() - larguraTexto) / 2;
         this.g.setColor(color);
         this.g.drawString(text, x, y);
     }
