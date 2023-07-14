21c52d7e8f5ee945e36fa57a5dcc81af7d45e685
TiagoHucs <tiagohucs@gmail.com>
2023-07-14 10:34:50 -0300

    organizando tudo para modulo game para depois levar o que interessa para engine

---
+++ game/src/main/java/game/PointEffect.java
@@ -0,0 +1,77 @@
+package game;
+
+
+import entities.Ator;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.awt.*;
+
+@Getter
+@Setter
+@Builder
+public class PointEffect {
+
+    private int x, y;
+    private int life;
+    private String text;
+    private Color color;
+
+    public static PointEffectBuilder scoreBuilder(Ator ator, int pts) {
+
+        float px = ator.getPosition().x + (ator.getSize().x / 2.0f);
+        float py = ator.getPosition().y;
+
+        return PointEffect.builder()
+                .life(10)
+                .color(Color.YELLOW)
+                .text(pts + "pts")
+                .x((int) px)
+                .y((int) py);
+    }
+
+    public static PointEffectBuilder extraLifeBuilder(Ator ator) {
+
+        float px = ator.getPosition().x + (ator.getSize().x / 2.0f);
+        float py = ator.getPosition().y;
+
+        return PointEffect.builder()
+                .life(100)
+                .color(Color.CYAN)
+                .text("Extra Life!")
+                .x((int) px)
+                .y((int) py);
+    }
+
+    public static PointEffectBuilder powerUpBuilder(Ator ator) {
+
+        float px = ator.getPosition().x + (ator.getSize().x / 2.0f);
+        float py = ator.getPosition().y;
+
+        return PointEffect.builder()
+                .life(50)
+                .color(Color.GREEN)
+                .text("Power Up!")
+                .x((int) px)
+                .y((int) py);
+    }
+
+    public static PointEffectBuilder powerDownBuilder(Ator ator) {
+
+        float px = ator.getPosition().x + (ator.getSize().x / 2.0f);
+        float py = ator.getPosition().y;
+
+        return PointEffect.builder()
+                .life(50)
+                .color(Color.RED)
+                .text("Danger!")
+                .x((int) px)
+                .y((int) py);
+    }
+
+    public void update() {
+        life--;
+        y--;
+    }
+}
