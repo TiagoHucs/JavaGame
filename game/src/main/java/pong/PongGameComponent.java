3b312550965079d725b7d4e99723ba5fabf28bc7
TiagoHucs <tiagohucs@gmail.com>
2023-07-16 12:19:33 -0300

    pong sample

--- game/src/main/java/pong/PongGameComponent.java
+++ game/src/main/java/pong/PongGameComponent.java
@@ -1,11 +1,46 @@
 package pong;
 
-import game.*;
+import entities.Actor;
+import game.GameComponent;
 import utilities.Config;
+
+import java.awt.*;
+import java.awt.geom.Point2D;
 
 public class PongGameComponent extends GameComponent {
 
+    private Actor ball = new Actor();
+    private float f = 5.0f;
+    private float x = f;
+    private float y = f;
+
+
     public PongGameComponent(Config cfg) {
         super(cfg);
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(Color.BLACK);
+        g.fillRect(0,0,getCfg().getGameWidth(),getHeight());
+        g.setColor(Color.WHITE);
+        g.fillRect(ball.getPositionWithOffsetX(),ball.getPositionWithOffsetY(),10,10);
+    }
+
+    @Override
+    public void update() {
+        ball.setVelocity(new Point2D.Float(x, y));
+        ball.move();
+        if(ball.getPosition().getY() > getCfg().getGameHeight()){
+            y = -f;
+        } else if(ball.getPosition().getY() < 0){
+            y = f;
+        }
+        if(ball.getPosition().getX() > getCfg().getGameWidth()){
+            x = -f;
+        } else if(ball.getPosition().getX() < 0){
+            x = f;
+        }
     }
 }
